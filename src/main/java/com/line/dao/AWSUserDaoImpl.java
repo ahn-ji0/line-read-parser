4377338139b5c15bc209d19a4e476dddc73881b9
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-18 13:40:58 +0900

    aws user dao implementation

--- src/main/java/com/line/dao/AWSUserDaoImpl.java
+++ src/main/java/com/line/dao/AWSUserDaoImpl.java
@@ -7,6 +7,7 @@ public class AWSUserDaoImpl extends UserDao {
 
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
+
         Class.forName("com.mysql.cj.jdbc.Driver");
         Map<String,String> env = System.getenv();
         Connection conn = DriverManager.getConnection(env.get("DB_HOST"),env.get("DB_USER"),env.get("DB_PASSWORD"));
