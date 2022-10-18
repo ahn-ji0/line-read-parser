f11f63d016ea27b01600aa064744ded0a97f8646
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-18 13:39:22 +0900

    domain

---
+++ src/main/java/com/line/context/User.java
@@ -0,0 +1,30 @@
+package com.line.context;
+
+//user data object(value object)
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/com/line/dao/UserDao.java
@@ -0,0 +1,42 @@
+package com.line.dao;
+
+import com.line.context.User;
+
+import java.sql.*;
+
+public abstract class UserDao {
+
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
+
+    public void add(User user) throws SQLException, ClassNotFoundException {
+        Connection conn = getConnection();
+        PreparedStatement pstm = conn.prepareStatement("INSERT INTO users(id,name,password) VALUES (?,?,?)");
+        pstm.setString(1,user.getId());
+        pstm.setString(2,user.getName());
+        pstm.setString(3, user.getPassword());
+
+        int status = pstm.executeUpdate();
+        System.out.println(status);
+
+        pstm.close();
+        conn.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection conn = getConnection();
+
+        PreparedStatement pstm = conn.prepareStatement("SELECT id, name, password FROM users WHERE id = ?");
+        pstm.setString(1,id);
+
+        ResultSet rs = pstm.executeQuery();
+        rs.next();
+        User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
+
+        rs.close();
+        pstm.close();
+        conn.close();
+
+        return user;
+    }
+
+}
