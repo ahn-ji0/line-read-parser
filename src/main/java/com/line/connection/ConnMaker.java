36aaee6f640d9341c4fa2be0119e7efab0a48418
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-19 11:38:50 +0900

    interface

--- src/main/java/com/line/connection/ConnMaker.java
+++ src/main/java/com/line/connection/ConnMaker.java
@@ -1,7 +1,8 @@
 package com.line.connection;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface ConnMaker {
-    java.sql.Connection connect() throws ClassNotFoundException, SQLException;
+    Connection connect() throws ClassNotFoundException, SQLException;
 }
