4e95951527f05e42c1f7a92a0390fc40146d5b05
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-19 13:40:35 +0900

    spring 적용

--- src/main/java/com/line/dao/UserDaoFactory.java
+++ src/main/java/com/line/dao/UserDaoFactory.java
@@ -2,8 +2,13 @@ package com.line.dao;
 
 import com.line.connection.AWSConnMaker;
 import com.line.dao.classExample.AWSConnectionMaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class UserDaoFactory {
+
+    @Bean
     public UserDao awsUserDao(){
         AWSConnMaker awsConnMaker = new AWSConnMaker();
         UserDao userDao = new UserDao(awsConnMaker);
--- src/test/java/com/line/dao/UserDaoTest.java
+++ src/test/java/com/line/dao/UserDaoTest.java
@@ -6,20 +6,31 @@ import com.line.context.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.assertj.ApplicationContextAssert;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 // ../connection의 인터페이스 ConnMaker를 이용하여 connection 분리
 // AWSConnMaker는 ConnMaker를 implement 하였음.
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     @DisplayName("DB쿼리 실행")
 
     void addAndSelect() throws SQLException, ClassNotFoundException {
 
-        UserDao userDao = new UserDaoFactory().awsUserDao();
+        UserDao userDao = context.getBean("awsUserDao",UserDao.class);
 
         userDao.deleteAll();
 
